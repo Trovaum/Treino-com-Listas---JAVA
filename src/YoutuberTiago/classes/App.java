aba712378599e5896f51a78aaa45f3ebe027c4f9
Trovaum <ricardopouza@gmail.com>
2021-12-27 06:37:19 -0300

    exercicios do youtuber<Arrays>

--- src/YoutuberTiago/classes/App.java
+++ src/YoutuberTiago/classes/App.java
@@ -2,16 +2,10 @@ package YoutuberTiago.classes;
 
 public class App {
     public static void main(String[] args) {
-        User[] users = new User[10];
-
-        for (int i = 0; i < users.length ; i++ ){
-            User userActual = new User();
-            userActual.setFirstName("Nome: " + i);
-            userActual.setLastName("Sobrenome: " + i);
-            users[i] = userActual;
-        }
-
+      User[] users= new User[]{
+              new User("Ricardo", "Pouza"),
+              new User("Enzo", "Pouza")
+      };
         System.out.println(users[1].getFirstName());
-        System.out.println(users[1].getLastName());
     }
 }
