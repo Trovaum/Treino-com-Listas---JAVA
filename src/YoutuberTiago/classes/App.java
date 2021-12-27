3767d14e71e0ecb4c935409df2d738d243a9c635
Trovaum <ricardopouza@gmail.com>
2021-12-27 06:55:14 -0300

    Array Add method

--- src/YoutuberTiago/classes/App.java
+++ src/YoutuberTiago/classes/App.java
@@ -1,11 +1,30 @@
 package YoutuberTiago.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-      User[] users= new User[]{
-              new User("Ricardo", "Pouza"),
-              new User("Enzo", "Pouza")
-      };
-        System.out.println(users[1].getFirstName());
+        //cria uma lista
+        List<User> users = new ArrayList<>();
+
+        // cria a se 10 registros
+        int i = 0;
+        while (i < 10){
+            User actual = new User("Nome" + i, "Sobrenome" + i);
+            users.add(actual);
+            i++;
+        }
+        System.out.println(users.get(8).getFirstName());
+        System.out.println(users.get(8).getLastName());
+
+        // adiciona mais um registro
+        User users11 = new User("Nome 11", "sobrenome 11");
+        users.add(users11);
+
+        System.out.println(users.get(10).getFirstName());
+        System.out.println(users.get(10).getLastName());
+
+
     }
 }
